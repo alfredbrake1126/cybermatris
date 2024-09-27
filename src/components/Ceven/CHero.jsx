d7e96bff3c542e312594ea0284187e41ecde96c5
tombrain1011 <tombrain1011@gmail.com>
2024-09-27 08:24:23 -0700

    fixed

--- src/components/Ceven/CHero.jsx
+++ src/components/Ceven/CHero.jsx
@@ -11,11 +11,12 @@ function CHero() {
       <div className="mx-auto flex items-center justify-center relative w-full  flex-col space-y-4 sm:space-y-6 md:space-y-8 mt-8 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-24 px-4 sm:px-6 md:px-8">
         <h1 className="font-roboto uppercase text-center text-2xl sm:text-3xl mt-20 md:text-4xl lg:text-5xl xl:text-6xl font-semibold max-w-7xl mx-auto z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-600 via-neutral-600 to-neutral-600 dark:from-neutral-800 dark:via-white dark:to-white bg-richBlack">
           Transforming Ideas into Innovative Software{" "}
-          <span className="text-platinum">
-            <Cover> Across All Platforms</Cover>
-          </span>
+          <br/>Across All Platforms
+          
+          {/* <span className="text-platinum">
+            <Cover>Across All Platforms</Cover>
+          </span> */}
         </h1>
-
         {/* Description */}
         <p className="text-white text-center font-normal text-sm sm:text-base md:text-lg lg:text-xl leading-relaxed md:leading-loose max-w-3xl mx-auto">
           Our development doesn't stop until our{" "}
--- src/components/Ceven/CTestimonials.jsx
+++ src/components/Ceven/CTestimonials.jsx
@@ -106,11 +106,12 @@ export default function CTestimonials() {
 
 // Small utility to highlight the content of specific section of a testimonial content
 export const Highlight = ({ children, className }) => {
+  console.log("classname", className);
+  
   return (
     <span
       className={cn(
         "font-bold bg-emerald-100 text-emerald-700 dark:bg-richBlack dark:text-emerald-500 px-1 py-0.5",
-        className
       )}
     >
       {children}
